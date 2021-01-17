c4f981ae530e7e1413a93edd917f9eb2c72e40b9
nclsbayona <nbayonaord@gmail.com>
2021-01-17 18:05:43 -0500

    .

--- Tkinter.py
+++ Tkinter.py
@@ -2,7 +2,7 @@ from tkinter import filedialog, Tk
 
 def getImageLocation() -> str:
     root = Tk()
-    root.filename =  filedialog.askopenfilename(initialdir = ".",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+    root.filename =  filedialog.askopenfilename(initialdir = ".",title = "Select file",filetypes = (("jpeg files","*.jpg"),("png files","*.png"), ("all files", "*")))
     return root.filename
 
 if __name__ == "__main__":
